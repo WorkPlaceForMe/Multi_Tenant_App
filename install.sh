9fbfdcca89344399e7a20b0a91bc62d5093063b8
ridweng <50190019+ridweng@users.noreply.github.com>
2020-11-03 13:18:56 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt-get install nginx
-sudo nano /etc/nginx/sites-available/default
-
-sudo rm /etc/nginx/sites-enabled/default
-sudo service nginx restart
-
-sudo systemctl status nginx.service
