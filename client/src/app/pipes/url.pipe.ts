0810221fa715fed1001723d157e9f311c452995f
Alex Kaiser <i93kaiser@hotmail.com>
2020-12-21 18:38:02 -0300

    changed " to '

--- client/src/app/pipes/url.pipe.ts
+++ client/src/app/pipes/url.pipe.ts
@@ -1,8 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from "@angular/platform-browser";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'url'
+  name: 'url',
 })
 export class UrlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) { }
