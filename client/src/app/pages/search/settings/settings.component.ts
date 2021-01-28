aea2a2ca555a70991daf6d17e23d521584cbe0ef
Alex Kaiser <i93kaiser@hotmail.com>
2021-01-28 16:36:46 -0300

    support of image

--- client/src/app/pages/search/settings/settings.component.ts
+++ client/src/app/pages/search/settings/settings.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { ColorsService } from '../../../services/colors';
 import { FacesService } from '../../../services/faces.service';
 
 @Component({
@@ -11,12 +13,20 @@ export class SettingsComponent implements OnInit {
 
   constructor(
     private facesService: FacesService, 
-    private activatedRoute: ActivatedRoute
-    ) { }
+    private activatedRoute: ActivatedRoute,
+    private colo: ColorsService,
+    private rd: Renderer2,
+    private sanitizer: DomSanitizer,
+    ) {
+      this.link = sanitizer.bypassSecurityTrustStyle('url(' + this.heatmap_pic + ')');
+     }
 
+  heatmap_pic: string;
+  width: number;
+  height: number;
   param = this.activatedRoute.snapshot.params.uuid;
   setting: Object = {};
-
+  link: SafeResourceUrl;
   algos: any = [
     { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false }, { activated: false },{ activated: false }
   ];
@@ -26,6 +36,12 @@ export class SettingsComponent implements OnInit {
   actA: number;
   actB: number;
   actC: number;
+  polygons = [];
+  perimeter = [];
+  complete = false;
+  private canvas;
+  private ctx;
+  @ViewChild('polygon', { static: true }) private polygon: ElementRef;
 
   ngOnInit(): void {
     this.facesService.getAlgos()
@@ -57,6 +73,8 @@ export class SettingsComponent implements OnInit {
       },
       err => console.error(err)
     )
+    this.setBcg();
+    this.complete = true;
   }
 
   getNbOccur(boolean: boolean, arr) {
@@ -68,4 +86,86 @@ export class SettingsComponent implements OnInit {
     return occurs;
   }
 
+  comparator(a, b) {
+    if (a[a.length - 1] < b[b.length - 1]) return -1;
+    if (a[a.length - 1] > b[b.length - 1]) return 1;
+    return 0;
+  }
+
+  public onChange(algorithm) {
+    if (algorithm.activated === false) {
+      for (let i = 0; i < this.polygons.length; i++) {
+        if (this.polygons[i][this.polygons[i].length - 1] === algorithm.id) {
+          this.polygons[i].push(1);
+        } else {
+          this.polygons[i].push(0);
+        }
+      }
+      this.polygons.sort(this.comparator);
+      for (let i = 0; i < this.polygons.length; i++) {
+        if (this.polygons[i][this.polygons[i].length - 1] === 1) {
+          this.polygons.pop();
+          i--;
+        }
+      }
+      for (let i = 0; i < this.polygons.length; i++) {
+        this.polygons[i].pop();
+      }
+      this.perimeter = [];
+      this.complete = true;
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.re_draw(true);
+    } else if (algorithm.activated === true) {
+      this.complete = true;
+    }
+    this.actA = this.getNbOccur(true, this.Aalgos);
+    this.actB = this.getNbOccur(true, this.Balgos);
+    this.actC = this.getNbOccur(true, this.Calgos);
+  }
+  private setBcg() {
+    // this.src = 'assets/heatmap_picture.png';
+    this.canvas = this.rd.selectRootElement(this.polygon['nativeElement']);
+    this.ctx = this.canvas.getContext('2d');
+    // let bcg = new Image();
+    // bcg.src = this.src;
+    // bcg.onload = () => {
+    //   this.ctx.drawImage(bcg, 0, 0, this.canvas.width, this.canvas.height)
+    // };
+  }
+
+  re_draw(end: boolean) {
+    let pred_colour, pred_fill;
+    for (let e = 0; e < this.polygons.length; e++) {
+      for (let u = 0; u < this.algos.length; u++) {
+        if (this.polygons[e][this.polygons[e].length - 1] === u) {
+          const rgb = this.colo.ran_col(u, this.algos.length);
+          pred_colour = 'rgba(' + rgb + ',1)';
+          pred_fill = 'rgba(' + rgb + ',0.3)';
+        }
+      }
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = 'yellow';
+      this.ctx.lineCap = 'circle';
+      this.ctx.beginPath();
+      for (let i = 0; i < this.polygons[e].length; i++) {
+        if (i === 0) {
+          this.ctx.moveTo(this.polygons[e][i]['x'], this.polygons[e][i]['y']);
+          this.ctx.fillStyle = pred_colour;
+          this.ctx.fillRect(this.polygons[e][i]['x'] - 2, this.polygons[e][i]['y'] - 2, 4, 4);
+        } else {
+          this.ctx.lineTo(this.polygons[e][i]['x'], this.polygons[e][i]['y']);
+          this.ctx.fillStyle = pred_colour;
+          this.ctx.fillRect(this.polygons[e][i]['x'] - 2, this.polygons[e][i]['y'] - 2, 4, 4);
+        }
+      }
+      if (end) {
+        this.ctx.lineTo(this.polygons[e][0]['x'], this.polygons[e][0]['y']);
+        this.ctx.closePath();
+        this.ctx.fillStyle = pred_fill;
+        this.ctx.fill();
+        this.ctx.strokeStyle = pred_colour;
+      }
+      this.ctx.stroke();
+    }
+  }
 }
