865fd7c32416e39d3d851ba1c3910920a682b9e3
Alex Kaiser <i93kaiser@hotmail.com>
2021-02-04 15:46:35 -0300

    Add and search data endpoints

--- server/app/controllers/elastic.controller.js
+++ server/app/controllers/elastic.controller.js
@@ -13,6 +13,7 @@ const client = new elasticsearch.Client({
   apiVersion: '7.x' // use the same version of your Elasticsearch instance
 })
 const { Op } = require('sequelize')
+const con = require('../models/dbmysql')
 const path =
   process.env.home + process.env.username + process.env.pathDocker + process.env.resources
 const multer = require('multer')
@@ -235,7 +236,7 @@ exports.delVid = (req, res) => {
         if (err) return res.status(500).json({ success: false, mess: err })
       })
     }
-    console.log('Debug Data : ', data);
+    console.log('Debug Data : ', data)
 
     Camera.destroy({
       where: { id: data.uuid, id_branch: decoded.id_branch, stored_vid: 'Yes' }
@@ -268,4 +269,133 @@ exports.editVid = (req, res) => {
         res.status(500).send({ success: false, message: err.message })
       })
   })
+}
+
+exports.some = async (req, res) => {
+  const table = 'intrude'
+  client.cluster.health({}, async function (_err, resp, status) {
+    console.log('-- Client Health --', resp)
+    try {
+      const r = await deleteIndex(table)
+      res.status(200).json({ success: true, data: r })
+    } catch (err) {
+      res.status(500).json({ success: false, mess: err })
+    }
+  })
+}
+
+async function deleteIndex (table) {
+  client.indices.delete({ index: table }, async function (_err, resp, status) {
+    console.log('delete', resp)
+    await createIndex(table)
+  })
+}
+async function createIndex (table) {
+  client.indices.create(
+    {
+      index: table
+    },
+    async function (err, resp, status) {
+      if (err) {
+        console.log(err)
+      } else {
+        // console.log('create', resp)
+        await readMysql(table)
+      }
+    }
+  )
+}
+
+async function readMysql (table) {
+  const sql = 'select * from ' + table + ';'
+  con.con().query(sql, async function (err, result) {
+    if (err) throw err
+    console.log(result.length)
+    result.forEach(async o => {
+      const jsonStr = JSON.stringify(o)
+      await saveToES(jsonStr, table)
+    })
+
+    setTimeout(printHourlyData, 10000)
+    setTimeout(printDailyData, 10000)
+    // pringData('day');
+  })
+}
+
+function saveToES (o, table) {
+  client.index(
+    {
+      index: table,
+      type: 'alerts',
+      body: o
+    },
+    function (_err, resp, status) {
+      return resp
+    }
+  )
+}
+
+function printHourlyData () {
+  printData('hour')
+}
+
+function printDailyData () {
+  printData('day')
+}
+
+function printData (interval, table) {
+  client.search(
+    {
+      index: table,
+      type: 'alerts',
+      body: {
+        aggs: {
+          simpleDatehHistogram: {
+            date_histogram: {
+              field: 'time',
+              interval: interval
+            }
+          }
+        }
+      }
+    },
+    function (error, response, status) {
+      if (error) {
+        console.log('search error: ' + error)
+      } else {
+        console.log(
+          '================================ Data by ' +
+            interval +
+            '============================================'
+        )
+        response.aggregations.simpleDatehHistogram.buckets.forEach(function (hit) {
+          console.log(hit)
+        })
+      }
+    }
+  )
+}
+
+exports.loit = async (req, res) => {
+  const interval = 'day'
+  const table = 'loitering'
+  try {
+    const search = await client.search({
+      index: table,
+      type: 'alerts',
+      body: {
+        aggs: {
+          simpleDatehHistogram: {
+            date_histogram: {
+              field: 'time',
+              interval: interval
+            }
+          }
+        }
+      }
+    })
+    res.status(200).json({ success: true, data: search.aggregations.simpleDatehHistogram.buckets })
+  } catch (err) {
+    res.status(500).json({ success: false, mess: err })
+  }
 }
--- server/app/models/dbmysql.js
+++ server/app/models/dbmysql.js
@@ -1,32 +1,40 @@
-var mysql=require('mysql');
-require('dotenv').config({ path: '../../config.env'});
+const mysql = require('mysql')
+require('dotenv').config({ path: '../../config.env' })
 
-var connection = {
-    con:function(){
-    if(!this.pool){
-        this.pool =  mysql.createPool({
-            host     : process.env.HOST,
-            user     : process.env.USERM,
-            password : process.env.PASSWORD,
-            database : process.env.DB,
-            connectionLimit: 40,
-        });
+const connection = {
+  con: function () {
+    if (!this.pool) {
+      this.pool = mysql.createPool({
+        host: process.env.HOST,
+        user: process.env.USERM,
+        password: process.env.PASSWORD,
+        database: process.env.DB,
+        connectionLimit: 40
+      })
 
-        this.pool.on('acquire', function (connection) {
-            console.log('Connection %d acquired. No of connnections: %d', connection.threadId, this._acquiringConnections.length+1);
-        });
+      this.pool.on('acquire', function (connection) {
+        console.log(
+          'Connection %d acquired. No of connnections: %d',
+          connection.threadId,
+          this._acquiringConnections.length + 1
+        )
+      })
 
-        this.pool.on('connection', function (connection) {
-            console.log('New connection created. No of connections: %d', this._acquiringConnections.length+1)
-        });
-        this.pool.on('enqueue', function () {
-            console.log('Waiting for available connection slot');
-        });
-        this.pool.on('release', function (connection) {
-            console.log('Connection %d released', connection.threadId);
-        });
+      this.pool.on('connection', function (connection) {
+        console.log(
+          'New connection created. No of connections: %d',
+          this._acquiringConnections.length + 1
+        )
+      })
+      this.pool.on('enqueue', function () {
+        console.log('Waiting for available connection slot')
+      })
+      this.pool.on('release', function (connection) {
+        console.log('Connection %d released', connection.threadId)
+      })
     }
-    return this.pool;
- }}
- 
-  module.exports=connection;
+    return this.pool
+  }
+}
+
+module.exports = connection
