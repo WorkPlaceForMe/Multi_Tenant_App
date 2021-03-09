665eddcebed1212bbf18d431e9af72138a9c7ca8
Alex Kaiser <i93kaiser@hotmail.com>
2021-03-09 14:24:57 -0300

    Heatmap from back end

--- server/app/controllers/heatmap.controller.js
+++ server/app/controllers/heatmap.controller.js
@@ -1,25 +1,32 @@
-require('dotenv').config({ path: '../../config.env'});
-var jwt = require("jsonwebtoken");
-var db=require('../models/dbmysql');
+require('dotenv').config({ path: '../../config.env' })
+const jwt = require('jsonwebtoken')
+const db = require('../models/dbmysql')
 
+exports.getAllHm = (req, res) => {
+  const token = req.headers['x-access-token']
 
-exports.getAllHm = (req, res) =>{
-    let token = req.headers["x-access-token"];
-
-    jwt.verify(token, process.env.secret, (err, decoded) => {
-       db.con().query("SELECT DATE_FORMAT(timestamp,'%d/%m/%y %H:%i') as date, x, y FROM heatmap where cam_id = ?;",[req.params.id], function (err, result) {
-        if (err) return res.status(500).json({success: false, message: err});
-        res.status(200).json({success: true, data: result})
-      });
+  jwt.verify(token, process.env.secret, (err, decoded) => {
+    db.con().query(
+      "SELECT DATE_FORMAT(timestamp,'%d/%m/%y %H:%i') as date, x_coordinate, y_coordinate FROM heatmap where cam_id = ?;",
+      [req.params.id],
+      function (err, result) {
+        if (err) return res.status(500).json({ success: false, message: err })
+        res.status(200).json({ success: true, data: result })
+      }
+    )
   })
 }
 
-exports.getHm = (req, res) =>{
-    let token = req.headers["x-access-token"];
-    jwt.verify(token, process.env.secret, (err, decoded) => {
-        db.con().query("SELECT DATE_FORMAT(timestamp,'%d/%m/%y %H:%i') as date, x, y, dwell FROM heatmap where timestamp >= ? and  timestamp <= ? and cam_id = ? order by date asc;",[req.params.st, req.params.end, req.params.id], function (err, result) {
-        if (err) return res.status(500).json({success: false, message: err});
-        res.status(200).json({success: true, data: result})
-      });
+exports.getHm = (req, res) => {
+  const token = req.headers['x-access-token']
+  jwt.verify(token, process.env.secret, (err, decoded) => {
+    db.con().query(
+      "SELECT DATE_FORMAT(timestamp,'%d/%m/%y %H:%i') as date, x_coordinate, y_coordinate, dwell FROM heatmap where timestamp >= ? and  timestamp <= ? and cam_id = ? order by date asc;",
+      [req.params.st, req.params.end, req.params.id],
+      function (err, result) {
+        if (err) return res.status(500).json({ success: false, message: err })
+        res.status(200).json({ success: true, data: result })
+      }
+    )
   })
 }
