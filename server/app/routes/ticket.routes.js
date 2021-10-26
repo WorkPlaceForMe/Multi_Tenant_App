f7891ec667d1b1fa804aaafaafd76c9b528bc19d
Alex Kaiser <i93kaiser@hotmail.com>
2021-10-26 13:39:31 -0300

    Eslint

--- server/app/routes/ticket.routes.js
+++ server/app/routes/ticket.routes.js
@@ -1,57 +1,46 @@
-//tickets.routes.js
+const controller = require('../controllers/ticket.controller')
+const { authJwt } = require('../middleware')
 
-const controller = require("../controllers/ticket.controller");
-const { authJwt } = require("../middleware");
-
-
-module.exports = function(app) {
-   app.use(function(req, res, next) {
-     res.header(
-       "Access-Control-Allow-Headers",
-       "x-access-token, Origin, Content-Type, Accept"
-     );
-     next();
-   });
-//app.post
-   app.get(
-    "/api/ticket/all/",
-    [authJwt.verifyToken, authJwt.isClientOrBranch],
-    controller.getAll
-  );
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept')
+    next()
+  })
+  app.get('/api/ticket/all/', [authJwt.verifyToken, authJwt.isClientOrBranch], controller.getAll)
 
   app.get(
-    "/api/ticket/search/all/",
+    '/api/ticket/search/all/',
     [authJwt.verifyToken, authJwt.isClientOrBranch],
     controller.searchAllTickets
-  );
+  )
 
   app.get(
-    "/api/ticket/alerts/overview/",
+    '/api/ticket/alerts/overview/',
     [authJwt.verifyToken, authJwt.isClientOrBranch],
     controller.alertsOverview
-  );
+  )
 
   app.post(
-    "/api/ticket/counts/",
+    '/api/ticket/counts/',
     [authJwt.verifyToken, authJwt.isClientOrBranch],
     controller.countTypes
-  );
+  )
 
   app.post(
-    "/api/ticket/some/",
+    '/api/ticket/some/',
     [authJwt.verifyToken, authJwt.isClientOrBranch],
     controller.getPeriod
-  );
+  )
 
   app.put(
-    "/api/ticket/check/:id",
+    '/api/ticket/check/:id',
     [authJwt.verifyToken, authJwt.isClientOrBranch],
     controller.check
-  );
+  )
 
   app.put(
-    "/api/ticket/assign/:id",
+    '/api/ticket/assign/:id',
     [authJwt.verifyToken, authJwt.isClientOrBranch],
     controller.assign
-  );
+  )
 }
