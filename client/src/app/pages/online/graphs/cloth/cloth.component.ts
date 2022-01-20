f861a9f0e7935ffccb0609343477d28ce83c1960
ridweng <i93kaiser@hotmail.com>
2022-01-20 15:12:02 +0000

    Hydrabad

--- client/src/app/pages/online/graphs/cloth/cloth.component.ts
+++ client/src/app/pages/online/graphs/cloth/cloth.component.ts
@@ -182,7 +182,7 @@ export class ClothComponent implements OnInit, OnDestroy {
         filter: false,
       },
       sleeve_length: {
-        title: 'CLOTHING',
+        title: 'UPPERWEAR',
         type: 'string',
         filter: false,
       },
