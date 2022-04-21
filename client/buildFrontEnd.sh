02aee05c130dc76578d6957ed75caef4dff39672
ridweng <i93kaiser@hotmail.com>
2022-04-21 13:23:08 +0100

    algos added on front end

--- client/buildFrontEnd.sh
+++ client/buildFrontEnd.sh
@@ -2,3 +2,4 @@ rm -rf dist
 ng build --optimization=true --aot=true --build-optimizer=true --index=src/indexProd.html --configuration=production &&
 mv ./dist/indexProd.html ./dist/index.html &&
 docker build -t $1 .
+docker push $1
