e10becee9ec61d9eb364c5e3b35877def35c4a4d
ridweng <i93kaiser@hotmail.com>
2022-05-20 12:44:39 +0100

    Info improved

--- wsServer/controllers/info.controller.js
+++ wsServer/controllers/info.controller.js
@@ -5,6 +5,10 @@ exports.info = async (req, res) => {
     try{
         const data = await fs.readFile("package.json", 'binary');
         const ver = JSON.parse(data)
+        delete ver.scripts
+        delete ver.dependencies
+        delete ver.devDependencies
+        delete ver.main
         res.status(200).json({success: true, data: ver})
     }catch(err){
         res.status(500).json({success: false, error: err})
@@ -17,6 +21,6 @@ exports.infoMain = async (req, res) => {
         const response = await axios.get(endpoint)
         res.status(200).json({ success: true, mess: response.data.data})
       } catch (err) {
-        res.status(500).json({success: false, error: err})
+        res.status(500).json({success: false, error: err.code})
       }
 }
