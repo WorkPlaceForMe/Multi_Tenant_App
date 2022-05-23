9214d78bb1d603a2b0981b0dca8955d438a46500
ridweng <i93kaiser@hotmail.com>
2022-05-23 11:22:24 +0100

    Changed routes of analytics

--- client/src/app/services/analytics.service.ts
+++ client/src/app/services/analytics.service.ts
@@ -12,79 +12,79 @@ export class AnalyticsService {
   API_URL = api + '/analytics/';
 
   loitering(id: string, dates){
-    return this.http.post(`${this.API_URL}loitering/${id}`, dates);
+    return this.http.post(`${this.API_URL}2/${id}`, dates);
   }
 
   violence(id: string, dates){
-    return this.http.post(`${this.API_URL}violence/${id}`, dates);
+    return this.http.post(`${this.API_URL}19/${id}`, dates);
   }
 
   intrude(id: string, dates){
-    return this.http.post(`${this.API_URL}intrude/${id}`, dates);
+    return this.http.post(`${this.API_URL}17/${id}`, dates);
   }
 
   aod(id: string, dates){
-    return this.http.post(`${this.API_URL}aod/${id}`, dates);
+    return this.http.post(`${this.API_URL}16/${id}`, dates);
   }
 
   covered(id: string, dates){
-    return this.http.post(`${this.API_URL}covered/${id}`, dates);
+    return this.http.post(`${this.API_URL}20/${id}`, dates);
   }
 
   pc(id: string, dates){
-    return this.http.post(`${this.API_URL}pc/${id}`, dates);
+    return this.http.post(`${this.API_URL}12/${id}`, dates);
   }
 
   social(id: string, dates){
-    return this.http.post(`${this.API_URL}social/${id}`, dates);
+    return this.http.post(`${this.API_URL}21/${id}`, dates);
   }
 
   queue(id: string, dates){
-    return this.http.post(`${this.API_URL}queue/${id}`, dates);
+    return this.http.post(`${this.API_URL}22/${id}`, dates);
   }
 
   helm(id: string, dates){
-    return this.http.post(`${this.API_URL}helm/${id}`, dates);
+    return this.http.post(`${this.API_URL}6/${id}`, dates);
   }
 
   vault(id: string, dates){
-    return this.http.post(`${this.API_URL}vault/${id}`, dates);
+    return this.http.post(`${this.API_URL}24/${id}`, dates);
   }
 
   parking(id: string, dates){
-    return this.http.post(`${this.API_URL}parking/${id}`, dates);
+    return this.http.post(`${this.API_URL}4/${id}`, dates);
   }
 
   anpr(id: string, dates){
-    return this.http.post(`${this.API_URL}anpr/${id}`, dates);
+    return this.http.post(`${this.API_URL}13/${id}`, dates);
   }
 
   barrier(id: string, dates){
-    return this.http.post(`${this.API_URL}barrier/${id}`, dates);
+    return this.http.post(`${this.API_URL}25/${id}`, dates);
   }
 
   vehicle(id: string, dates){
-    return this.http.post(`${this.API_URL}vehicle/${id}`, dates);
+    return this.http.post(`${this.API_URL}26/${id}`, dates);
   }
 
   tamper(id: string, dates){
-    return this.http.post(`${this.API_URL}tamper/${id}`, dates);
+    return this.http.post(`${this.API_URL}27/${id}`, dates);
   }
 
   animal(id: string, dates){
-    return this.http.post(`${this.API_URL}animal/${id}`, dates);
+    return this.http.post(`${this.API_URL}28/${id}`, dates);
   }
 
   accident(id: string, dates){
-    return this.http.post(`${this.API_URL}accident/${id}`, dates);
+    return this.http.post(`${this.API_URL}29/${id}`, dates);
   }
 
   axle(id: string, dates){
-    return this.http.post(`${this.API_URL}axle/${id}`, dates);
+    return this.http.post(`${this.API_URL}30/${id}`, dates);
   }
 
   carmake(id: string, dates){
-    return this.http.post(`${this.API_URL}carmake/${id}`, dates);
+    return this.http.post(`${this.API_URL}31/${id}`, dates);
   }
 
   plate(id: string, dates){
@@ -92,108 +92,108 @@ export class AnalyticsService {
   }
 
   vcount(id: string, dates){
-    return this.http.post(`${this.API_URL}vcount/${id}`, dates);
+    return this.http.post(`${this.API_URL}33/${id}`, dates);
   }
 
   direction(id: string, dates){
-    return this.http.post(`${this.API_URL}direction/${id}`, dates);
+    return this.http.post(`${this.API_URL}8/${id}`, dates);
   }
 
   speeding(id: string, dates){
-    return this.http.post(`${this.API_URL}speeding/${id}`, dates);
+    return this.http.post(`${this.API_URL}5/${id}`, dates);
   }
   fr(id: string, dates){
-    return this.http.post(`${this.API_URL}fr/${id}`, dates);
+    return this.http.post(`${this.API_URL}0/${id}`, dates);
   }
   cloth(id: string, dates){
-    return this.http.post(`${this.API_URL}cloth/${id}`, dates);
+    return this.http.post(`${this.API_URL}32/${id}`, dates);
   }
   pcOnView(id: string, dates){
     return this.http.post(`${this.API_URL}pcOnView/${id}`, dates);
   }
   brand(id: string, dates){
-    return this.http.post(`${this.API_URL}brand/${id}`, dates);
+    return this.http.post(`${this.API_URL}34/${id}`, dates);
   }
   fire(id: string, dates){
-    return this.http.post(`${this.API_URL}fire/${id}`, dates);
+    return this.http.post(`${this.API_URL}39/${id}`, dates);
   }
   collapse(id: string, dates){
-    return this.http.post(`${this.API_URL}collapse/${id}`, dates);
+    return this.http.post(`${this.API_URL}38/${id}`, dates);
   }
   weapon(id: string, dates){
-    return this.http.post(`${this.API_URL}weapon/${id}`, dates);
+    return this.http.post(`${this.API_URL}35/${id}`, dates);
   }
   bottle(id: string, dates){
-    return this.http.post(`${this.API_URL}bottle/${id}`, dates);
+    return this.http.post(`${this.API_URL}36/${id}`, dates);
   }
   wavingHands(id: string, dates){
-    return this.http.post(`${this.API_URL}wavingHands/${id}`, dates);
+    return this.http.post(`${this.API_URL}41/${id}`, dates);
   }
   smoking(id: string, dates){
-    return this.http.post(`${this.API_URL}smoking/${id}`, dates);
+    return this.http.post(`${this.API_URL}42/${id}`, dates);
   }
   slapping(id: string, dates){
-    return this.http.post(`${this.API_URL}slapping/${id}`, dates);
+    return this.http.post(`${this.API_URL}44/${id}`, dates);
   }
   running(id: string, dates){
-    return this.http.post(`${this.API_URL}running/${id}`, dates);
+    return this.http.post(`${this.API_URL}46/${id}`, dates);
   }
   pushing(id: string, dates){
-    return this.http.post(`${this.API_URL}pushing/${id}`, dates);
+    return this.http.post(`${this.API_URL}50/${id}`, dates);
   }
   purse(id: string, dates){
-    return this.http.post(`${this.API_URL}purse/${id}`, dates);
+    return this.http.post(`${this.API_URL}48/${id}`, dates);
   }
   pullingHair(id: string, dates){
-    return this.http.post(`${this.API_URL}pullingHair/${id}`, dates);
+    return this.http.post(`${this.API_URL}40/${id}`, dates);
   }
   following(id: string, dates){
-    return this.http.post(`${this.API_URL}following/${id}`, dates);
+    return this.http.post(`${this.API_URL}49/${id}`, dates);
   }
   disrobing(id: string, dates){
-    return this.http.post(`${this.API_URL}disrobing/${id}`, dates);
+    return this.http.post(`${this.API_URL}47/${id}`, dates);
   }
   crowd(id: string, dates){
-    return this.http.post(`${this.API_URL}crowd/${id}`, dates);
+    return this.http.post(`${this.API_URL}43/${id}`, dates);
   }
   blocking(id: string, dates){
-    return this.http.post(`${this.API_URL}blocking/${id}`, dates);
+    return this.http.post(`${this.API_URL}45/${id}`, dates);
   }
   transpassing(id: string, dates){
-    return this.http.post(`${this.API_URL}transpassing/${id}`, dates);
+    return this.http.post(`${this.API_URL}52/${id}`, dates);
   }
   cameraDefocused(id: string, dates){
-    return this.http.post(`${this.API_URL}cameraDefocused/${id}`, dates);
+    return this.http.post(`${this.API_URL}53/${id}`, dates);
   }
   cameraBlinded(id: string, dates){
-    return this.http.post(`${this.API_URL}cameraBlinded/${id}`, dates);
+    return this.http.post(`${this.API_URL}54/${id}`, dates);
   }
   sceneChange(id: string, dates){
-    return this.http.post(`${this.API_URL}sceneChange/${id}`, dates);
+    return this.http.post(`${this.API_URL}55/${id}`, dates);
   }
   objectRemoval(id: string, dates){
-    return this.http.post(`${this.API_URL}objectRemoval/${id}`, dates);
+    return this.http.post(`${this.API_URL}56/${id}`, dates);
   }
   smokeDetection(id: string, dates){
-    return this.http.post(`${this.API_URL}smokeDetection/${id}`, dates);
+    return this.http.post(`${this.API_URL}57/${id}`, dates);
   }
   velocity(id: string, dates){
-    return this.http.post(`${this.API_URL}velocity/${id}`, dates);
+    return this.http.post(`${this.API_URL}58/${id}`, dates);
   }
   exit(id: string, dates){
-    return this.http.post(`${this.API_URL}exit/${id}`, dates);
+    return this.http.post(`${this.API_URL}60/${id}`, dates);
   }
   enterExit(id: string, dates){
-    return this.http.post(`${this.API_URL}enterExit/${id}`, dates);
+    return this.http.post(`${this.API_URL}59/${id}`, dates);
   }
   harrasment(id: string, dates){
-    return this.http.post(`${this.API_URL}harrasment/${id}`, dates);
+    return this.http.post(`${this.API_URL}61/${id}`, dates);
   }
   abduction(id: string, dates){
-    return this.http.post(`${this.API_URL}abduction/${id}`, dates);
+    return this.http.post(`${this.API_URL}62/${id}`, dates);
   }
   dir(id: string, dates){
-    return this.http.post(`${this.API_URL}dir/${id}`, dates);
+    return this.http.post(`${this.API_URL}63/${id}`, dates);
   }
   constructor(private http: HttpClient) { }
 }
