caeb863d74b0179dace5c59b075cfb22a6adeba1
ridweng <i93kaiser@hotmail.com>
2022-07-13 15:19:01 +0100

    Assets for icon front end

--- client/buildFrontEnd.sh
+++ client/buildFrontEnd.sh
@@ -1,5 +1,4 @@
 rm -rf dist
 ng build --optimization=true --aot=true --build-optimizer=true --index=src/indexProd.html --configuration=production &&
-mv ./dist/indexProd.html ./dist/index.html &&
 docker build -t $1 .
 docker push $1
