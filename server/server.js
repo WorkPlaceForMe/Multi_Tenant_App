0465589889baab7e6b7be2d618ae0b53978ccfd6
Ranjit Bhakta <ranjit3t@gmail.com>
2022-07-19 23:02:19 +0530

    Creating end pint for serving static media

--- server/server.js
+++ server/server.js
@@ -203,6 +203,10 @@ require('./app/routes/info.routes')(app)
 // resources being served
 app.use('/api/pictures', express.static(picResourceFolderPath))
 
+// static media resources
+console.log('Media path: ' + path.join(__dirname, 'media'))
+app.use('/media', express.static(path.join(__dirname, 'media')));
+
 // app.use((req, res, next) => {
 //   res.sendFile('/app/views/error.html', { root: __dirname })
 // })
