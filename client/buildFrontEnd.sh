5e429d9daa67d8a9abe60bd6536f35f205fcd8f1
ridweng <i93kaiser@hotmail.com>
2022-07-22 12:57:40 +0200

    build front end updated

--- client/buildFrontEnd.sh
+++ client/buildFrontEnd.sh
@@ -1,4 +1,4 @@
 rm -rf dist
-ng build --optimization=true --aot=true --build-optimizer=true --index=src/indexProd.html --configuration=production &&
+ng build --optimization=true --aot=true --build-optimizer=true --configuration=production &&
 docker build -t $1 .
 docker push $1
