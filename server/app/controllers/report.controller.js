e3cdbf180b4dffebfef7b25d24a18c28aad2c148
ridweng <i93kaiser@hotmail.com>
2022-09-14 12:05:29 +0200

    report changed for people and alerts

--- server/app/controllers/report.controller.js
+++ server/app/controllers/report.controller.js
@@ -18,23 +18,27 @@ const createExelReport = (data, name) => {
 
 exports.report = async (req, res) => {
   const data = req.body
-
   let extra = ''
   if (req.params.algo_id === '22') {
     extra = " and severity = '2'"
   }
+  let query = `SELECT id, time, camera_name, cam_id, zone, severity from queue_alerts WHERE ${data.type} = '${req.params.cam_id}' and time >= '${data.start}' and  time <= '${data.end}'${extra} order by time asc;`
+
+  if (data.algo === 'count') {
+    query = `SELECT id, start_time, end_time, camera_name, cam_id, qid, track_id, queuing from queue_mgt WHERE ${data.type} = '${req.params.cam_id}' and start_time >= '${data.start}' and  start_time <= '${data.end}' order by start_time asc;`
+  }
   await db
     .con()
     .query(
-            `SELECT id, time, camera_name, cam_id, zone, severity from queue_alerts WHERE ${data.type} = '${req.params.cam_id}' and time >= '${data.start}' and  time <= '${data.end}'${extra} order by time asc;`,
-            async function (err, result) {
-              const fn = `${req.params.cam_id}-${data.start}`
-              const exc = await createExelReport(result, fn)
-              const readStream = new stream.PassThrough()
-              readStream.end(exc)
-              res.set('Content-disposition', 'attachment; filename=' + fn)
-              res.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+      query,
+      async function (_err, result) {
+        const fn = `${req.params.cam_id}-${data.start}`
+        const exc = await createExelReport(result, fn)
+        const readStream = new stream.PassThrough()
+        readStream.end(exc)
+        res.set('Content-disposition', 'attachment; filename=' + fn)
+        res.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-              readStream.pipe(res)
-            })
+        readStream.pipe(res)
+      })
 }
