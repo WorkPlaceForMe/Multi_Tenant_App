6a6af09dd7abbcad123556fb0e3048b34c719025
Ranjit Bhakta <ranjit3t@gmail.com>
2022-12-05 22:33:48 +0530

    Fixing auth guard issue which was revealing the content of protected page before denying the access with alert

--- client/src/app/guard/auth.guard.ts
+++ client/src/app/guard/auth.guard.ts
@@ -1,49 +1,46 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from "@angular/router";
 import { AuthService } from "../services/auth.service";
-import { Observable } from 'rxjs';
-import { FacesService } from '../services/faces.service';
+import { Observable } from "rxjs";
+import { FacesService } from "../services/faces.service";
+import { resolve } from "dns";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
   constructor(
     public authService: AuthService,
     public router: Router,
     public facesService: FacesService
-  ){ }
+  ) {}
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-    Observable<boolean> | Promise<boolean> | boolean {
-      if(this.authService.isLoggedIn !== true) {
-        return this.router.navigate(['pages/sign-in'])
-      }
-      this.facesService.mess().subscribe(
-        res => {},
-        err => {
-          console.log(err)
-              window.localStorage.clear();
-              window.sessionStorage.clear();
-              window.location.reload()
-              this.router.navigate(['/pages'])
-              window.alert("Your session has expired, please log in again.");
-        //   this.authService.signOut(JSON.parse(localStorage.getItem('now_user'))['username']).subscribe(
-        //     res=>{
-        //       window.localStorage.clear();
-        //       window.sessionStorage.clear();
-        //       // window.location.reload()
-        //       this.router.navigate(['/pages'])
-        //     }, err =>{ 
-        //       console.error(err)
-        //       window.localStorage.clear();
-        //       window.sessionStorage.clear();
-        //       // window.location.reload()
-        //       this.router.navigate(['/pages'])
-        //     }
-        // )
-        }
-      )
-      return true;
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isLoggedIn !== true) {
+      return this.router.navigate(["pages/sign-in"]);
+    } else {
+      return new Promise((resolve, reject) => {
+        this.facesService.mess().subscribe(
+          (res) => {
+            return resolve(true);
+          },
+          (err) => {
+            window.localStorage.clear();
+            window.sessionStorage.clear();
+            window.location.reload();
+
+            return resolve(false);
+          }
+        );
+      });
     }
+  }
 }
