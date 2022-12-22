7b94be23cb766cb05df7b50fd2216633a26f319a
ridweng <50190019+ridweng@users.noreply.github.com>
2022-12-22 13:42:13 -0300

    Delete buildFrontEnd.sh

--- client/buildFrontEnd.sh
+++
@@ -1,4 +0,0 @@
-rm -rf dist
-ng build --optimization=true --aot=true --build-optimizer=true --configuration=production &&
-docker build -t graymatics1/multi-tenant-ui:$1 .
-docker push graymatics1/multi-tenant-ui:$1
