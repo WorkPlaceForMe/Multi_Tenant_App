4a991031daa591be3e09a09d7c4ec75381dc7a36
ridweng <i93kaiser@hotmail.com>
2023-02-07 11:21:09 -0300

    proxy added in rep

---
+++ proxy/README.md
@@ -0,0 +1,13 @@
+# Websocket Server
+Server to communicate via websocket
+
+Installation:
+npm install
+
+Run:
+npm start
+
+Requires:
+config.env
+
+Default port 3301
---
+++ proxy/routes/proxy.route.js
@@ -0,0 +1,39 @@
+const { verifyIn, verify } = require('../middleware')
+const controller = require('../controllers/proxy.controller')
+
+module.exports = function (app, v) {
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept')
+    next()
+  })
+
+  app.post(
+    `/api/${v}/r`,
+    [verify.check],
+    controller.retrieve
+  )
+
+  app.put(
+    `/api/${v}/u`,
+    [verify.check],
+    controller.update
+  )
+
+  app.put(
+    `/api/${v}/um`,
+    [verify.check],
+    controller.updateMultiple
+  )
+  
+  app.post(
+    `/api/${v}/i`,
+    [verifyIn.values,verify.check],
+    controller.insert
+  )
+
+  app.post(
+    `/api/${v}/c`,
+    [verify.check],
+    controller.create
+  )
+}
